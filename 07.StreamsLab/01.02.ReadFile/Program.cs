a44b9725018a816acf251ea5fae43e8b6b1d5916
SonicTheCat <paveliliev90@gmail.com>
2018-09-18 03:36:13 +0300

    STREAMS

---
+++ 07.StreamsLab/01.02.ReadFile/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01._02.ReadFile
+{
+    using System;
+    using System.IO;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            using (var reader = new StreamReader("../../Program.cs"))
+            {
+                var rowNumber = 1;
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine("Line" + rowNumber++ + ": " + line);
+                }
+            }
+        }
+    }
+}
---
+++ 07.StreamsLab/02.test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _02.test
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var source = new FileStream("../../csharp.jpg",FileMode.Open))
+            {
+                using (var destination = new FileStream("../../copiedPhoto.jpg", FileMode.Create))
+                {
+                    var buffer = new byte[4096]; 
+                    while (true)
+                    {
+                        int readBytes = source.Read(buffer, 0, buffer.Length);
+                        if (readBytes == 0)
+                        {
+                            break; 
+                        }
+                        destination.Write(buffer, 0, readBytes); 
+                    }
+                }
+            }
+        }
+    }
+}
